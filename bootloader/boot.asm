1991f2701d363f6560f52a1c0c99c438548f9841
Martin Levesque <martin.levesque@epita.fr>
2025-04-15 17:34:22 +0200

    feat(bootloader): setup register for real mode

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -3,5 +3,14 @@
 
 KERNEL_OFFSET equ 0x1000 ;kernel start address
 
+jump 0x0000:start
+
 start:
     cli ;disable interrupt
+    xor ax, ax ;clear ax
+    mov ds, ax ;set data segment to 0
+    mov es, ax ;set extra segment to 0
+    mov ss, ax ;set stack segment to 0
+    mov fs, ax ;set fs segment to 0
+    mov gs, ax ;set gs segment to 0
+    mov sp, 0x7C00 ;set stack pointer to 0x7C00
